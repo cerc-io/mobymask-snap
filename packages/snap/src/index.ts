e0426706d96314411bd7039a5e9bbf82f0806e8e
Nabarun <nabarun@deepstacksoft.com>
2023-07-27 11:05:00 +0530

    Remove origin param from jsdoc

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -8,8 +8,6 @@ const SALT = 'Generate key';
  * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
  *
  * @param args - The request handler args as object.
- * @param args.origin - The origin of the request, e.g., the website that
- * invoked the snap.
  * @param args.request - A validated JSON-RPC request object.
  * @returns The result of `snap_dialog`.
  * @throws If the request method is not valid for this snap.
